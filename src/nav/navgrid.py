29bb8627ebb02cdbdd208b54a9c0060168a78347
Dennis Meuwissen <dennis.meuwissen@gmail.com>
2013-03-05 22:42:01 +0100

    Split NavArea and NavElement into their own modules.

--- src/nav/navgrid.py
+++ src/nav/navgrid.py
@@ -1,4 +1,5 @@
 from doom.mapenum import *
+from nav.navelement import NavElement
 from nav.walker import Walker
 import cPickle
 import pygame
@@ -44,54 +45,6 @@ reason_text = {
     REASON_LEAK: 'Grid leak'
 }
 
-
-class NavElement(object):
-    __slots__ = ('x', 'y', 'z', 'special_sector', 'flags', 'elements', 'area', 'index')
-    
-    def __init__(self, x, y, z):
-        self.x = x
-        self.y = y
-        self.z = z
-        
-        self.special_sector = -1
-        self.flags = 0
-        
-        self.elements = [None] * 4
-        self.area = None
-        
-        self.index = -1
-        
-        
-    def __repr__(self):
-        return 'element x {}, y {}, z {}, flags {}, sector {}'.format(self.x, self.y, round(self.z, 2), self.flags, self.special_sector)
-    
-    def __getstate__(self):
-        indices = [-1] * 4
-        for direction in DIRECTION_RANGE:
-            if self.elements[direction] is not None:
-                indices[direction] = self.elements[direction].index
-        
-        return [self.x, self.y, self.z, self.special_sector, self.flags, self.index, indices]
-    
-    def __setstate__(self, state):
-        self.x = state[0]
-        self.y = state[1]
-        self.z = state[2]
-        self.special_sector = state[3]
-        self.flags = state[4]
-        self.index = state[5]
-        self.elements = state[6]
-        self.area = None
-    
-    def __eq__(self, other):
-        if other is None:
-            return False
-        
-        return (self.special_sector == other.special_sector and self.flags == other.flags and self.z == other.z)
-    
-    def __ne__(self, other):
-        return not self.__eq__(other)
-     
 
 class NavGrid(object):
     
--- src/nav/navmesh.py
+++ src/nav/navmesh.py
@@ -1,3 +1,4 @@
+from nav.navarea import NavArea
 import pygame
 
 
@@ -11,40 +12,6 @@ SIDE_LEFT = 3
 SIDE_RANGE = [SIDE_TOP, SIDE_RIGHT, SIDE_BOTTOM, SIDE_LEFT]
 SIDE_RANGE_OPPOSITE = [SIDE_BOTTOM, SIDE_LEFT, SIDE_TOP, SIDE_RIGHT]
 
-
-class NavArea(object):
-    __slots__ = ('x1', 'y1', 'x2', 'y2', 'z', 'sector', 'flags', 'elements')
-
-
-    def __init__(self, x1, y1, x2, y2, z):
-        self.x1 = x1
-        self.y1 = y1
-        self.x2 = x2
-        self.y2 = y2
-        self.z = z
-        self.sector = -1
-        self.flags = 0
-        self.elements = []
-
-
-    def get_side(self, side):
-        if side == SIDE_TOP:
-            return self.x1, self.y1, self.x2, self.y1
-        
-        elif side == SIDE_RIGHT:
-            return self.x2, self.y1, self.x2, self.y2
-            
-        elif side == SIDE_BOTTOM:
-            return self.x1, self.y2, self.x2, self.y2
-            
-        elif side == SIDE_LEFT:
-            return self.x1, self.y1, self.x1, self.y2
-        
-        return None
-    
-    def __repr__(self):
-        return 'x1 {}, y1 {}, x2 {}, y2 {}, z {}, sector {}, width {}, height {}'.format(self.x1, self.y1, self.x2, self.y2, self.z, self.sector, self.x2 - self.x1, self.y2 - self.y1) 
-    
 
 class NavMesh(object):
     
