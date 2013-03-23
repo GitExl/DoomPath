31eed4a65026070faeac871f6c50e63dbcc9f07f
Dennis Meuwissen <dennis.meuwissen@gmail.com>
2013-03-23 16:02:08 +0100

    Remove extraneous function declaration.

--- src/lib.py
+++ src/lib.py
@@ -11,10 +11,6 @@ mapdata_free = doompathlib.mapdata_free
 mapdata_free.argtypes = [c_void_p]
 mapdata_free.restype = None
 
-mapdata_put_vertices = doompathlib.mapdata_put_vertices
-mapdata_put_vertices.argtypes = [c_void_p, c_uint, c_char_p]
-mapdata_put_vertices.restype = None
-
 mapdata_put_nodes = doompathlib.mapdata_put_nodes
 mapdata_put_nodes.argtypes = [c_void_p, c_uint, c_char_p]
 mapdata_put_nodes.restype = None
